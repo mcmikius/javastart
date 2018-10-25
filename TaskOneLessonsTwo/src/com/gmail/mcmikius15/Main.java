490e2dccadaabc101ab58f5a577e4b02c1b47372
Michail Bondarenko <mcmikius15@gmail.com>
2018-10-25 18:39:59 +0300

    done first task

--- TaskOneLessonsTwo/src/com/gmail/mcmikius15/Main.java
+++ TaskOneLessonsTwo/src/com/gmail/mcmikius15/Main.java
@@ -1,8 +1,37 @@
 package com.gmail.mcmikius15;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        int a;
+        int b;
+        int c;
+        int d;
+        int max;
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Input a: ");
+        a= scan.nextInt();
+        System.out.println("Input b: ");
+        b= scan.nextInt();
+        System.out.println("Input c: ");
+        c=scan.nextInt();
+        System.out.println("Input d: ");
+        d= scan.nextInt();
+        max=a;
+        if (c>max){
+            max=c;
+        }
+        if (b>max){
+            max=b;
+        }
+        if (d>max){
+            max=d;
+        }
+        System.out.println("Max =" +max );
+        scan.close();
     }
+
+
 }
