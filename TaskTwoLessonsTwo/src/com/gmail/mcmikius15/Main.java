d545a9d00f7c4ff856e1604611b6e111715fe94f
Michail Bondarenko <mcmikius15@gmail.com>
2018-10-25 19:24:11 +0300

    edit

--- TaskTwoLessonsTwo/src/com/gmail/mcmikius15/Main.java
+++ TaskTwoLessonsTwo/src/com/gmail/mcmikius15/Main.java
@@ -5,17 +5,29 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-      Scanner scan= new Scanner(System.in);
-    int appartmentNumber;
-      System.out.println("Please input appartment number: ");
-      appartmentNumber = scan.nextInt();
-    if (appartmentNumber <= 144) {
-      System.out.println("Enter# " +((appartmentNumber+35)/36));
-      System.out.println("Flor# "+((((appartmentNumber-1)%36)/4)+1));
+      
+    	Scanner scan= new Scanner(System.in);
+    
+    	int appartmentNumber;
+      
+    	System.out.println("Please input appartment number: ");
+      
+    	appartmentNumber = scan.nextInt();
+    
+    	if (appartmentNumber <= 144) {
+      
+    		System.out.println("Enter# " +((appartmentNumber+35)/36));
+      
+    		System.out.println("Flor# "+((((appartmentNumber-1)%36)/4)+1));
+    
+    	} else {
+      
+    		System.out.println("There is no such appartment");
+    
+    	}
+      
+    	scan.close();
+    
     }
-    else {
-      System.out.println("There is no such appartment");
-    }
-      scan.close();
-    }
+
 }
