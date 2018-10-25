b631e94cdd54d712caeb5ac9529f357b886bab27
Michail Bondarenko <mcmikius15@gmail.com>
2018-10-25 19:03:04 +0300

    Done two and create three

---
+++ TaskTwoLessonsTwo/src/com/gmail/mcmikius15/Main.java
@@ -0,0 +1,21 @@
+package com.gmail.mcmikius15;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+      Scanner scan= new Scanner(System.in);
+    int appartmentNumber;
+      System.out.println("Please input appartment number: ");
+      appartmentNumber = scan.nextInt();
+    if (appartmentNumber <= 144) {
+      System.out.println("Enter# " +((appartmentNumber+35)/36));
+      System.out.println("Flor# "+((((appartmentNumber-1)%36)/4)+1));
+    }
+    else {
+      System.out.println("There is no such appartment");
+    }
+    	scan.close();
+    }
+}
