6d081bc483647fc86f132a313bf465717ad13e07
Michail Bondarenko <mcmikius15@gmail.com>
2018-10-25 19:41:06 +0300

    edit one

--- TaskOneLessonsTwo/src/com/gmail/mcmikius15/Main.java
+++ TaskOneLessonsTwo/src/com/gmail/mcmikius15/Main.java
@@ -4,32 +4,48 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
-        int a;
+    public static void main(String[] args){
+        
+    	int a;
+    	
         int b;
+        
         int c;
+        
         int d;
+        
         int max;
+        
         Scanner scan = new Scanner(System.in);
-        System.out.println("Input a: ");
+        
+        System.out.println("Input a:");
+        
         a= scan.nextInt();
-        System.out.println("Input b: ");
+        System.out.println("Input b:");
+        
         b= scan.nextInt();
-        System.out.println("Input c: ");
+        System.out.println("Input c:");
+        
         c=scan.nextInt();
-        System.out.println("Input d: ");
+        System.out.println("Input d:");
+        
         d= scan.nextInt();
         max=a;
+        
         if (c>max){
             max=c;
         }
+        
         if (b>max){
             max=b;
         }
+        
         if (d>max){
             max=d;
         }
+        
         System.out.println("Max =" +max );
+        
         scan.close();
     }
 
