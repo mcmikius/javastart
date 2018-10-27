383a5f1c1688befad2ff5b8aa28b504a703fa600
Michail Bondarenko <mcmikius15@gmail.com>
2018-10-27 13:53:20 +0300

    end method create

---
+++ LessonSeven/src/com/gmail/mcmikius15/Main.java
@@ -0,0 +1,14 @@
+package com.gmail.mcmikius15;
+
+public class Main {
+	
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	
+
+}
---
+++ LessonSixA/src/com/gmail/mcmikius15/Main.java
@@ -0,0 +1,22 @@
+package com.gmail.mcmikius15;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int size = 10;
+		int [] a = new int [size];
+		
+		int [] b = new int[] {7, 15, -2, 7};
+		
+		int temp = b[2];
+		
+		System.out.println(temp);
+		
+		int arraySize = b.length;
+		System.out.println(arraySize);
+
+	}
+
+}
