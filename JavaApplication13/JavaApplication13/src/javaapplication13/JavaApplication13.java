47b9429f896dc6f3c8807410def8009f54edbef4
Michail Bondarenko <mcmikius15@gmail.com>
2018-11-03 14:44:29 +0200

    First course lectures is done

--- JavaApplication13/JavaApplication13/src/javaapplication13/JavaApplication13.java
+++ JavaApplication13/JavaApplication13/src/javaapplication13/JavaApplication13.java
@@ -19,10 +19,17 @@ public class JavaApplication13 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        String text = "Hello world";
-        File file = new File("hello.txt");
         
-        saveStringToFile(text, file);
+        String[] goods = new String[] {"Americano", "Latte", "Capuchinu", "Espresso", "tea"};
+        int[] price = new int[] {7, 8, 8, 8, 7};
+        int[] cups = new int[] {46, 69, 73, 52, 23};
+        
+        String report = getCSVReport(goods, price, cups);
+        
+        File file = new File("report.csv");
+        
+        saveStringToFile(report, file);
+
     }
     
     public static void saveStringToFile(String text, File file) {
@@ -31,6 +38,16 @@ public class JavaApplication13 {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+    
+    public static String getCSVReport(String[] goods, int[] price, int[] cups) {
+        String report = "";
+        for (int i = 0; i < goods.length; i++) {
+            report += goods[i] + ";" + price[i] + ";" + cups[i];
+            report += System.lineSeparator();
+        }
+        
+        return report;
     }
     
 }
